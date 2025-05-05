723b58121117ce01cdffcd615da0cc760a09483b
Soru <chansolchoe9216@gmail.com>
2025-05-06 01:50:28 +0900

    [fix] fix redirect origin error at deploy site

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from "next/server";
 import { createServerClient } from "@/lib/auth/server";
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url);
+  const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
-  const next = searchParams.get("next") ?? "/workspace"; //　ログイン後のリダイレクト先
+  const next = searchParams.get("next") ?? "/workspace";
 
   if (code) {
     const supabaseServerClient = await createServerClient();
@@ -12,17 +12,12 @@ export async function GET(request: Request) {
       code
     );
     if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host");
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
-        return NextResponse.redirect(`${origin}${next}`);
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
-      } else {
-        return NextResponse.redirect(`${origin}${next}`);
-      }
+      const redirectUrl = `${process.env.NEXT_PUBLIC_SITE_URL}${next}`;
+      return NextResponse.redirect(redirectUrl);
     }
   }
 
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  return NextResponse.redirect(
+    `${process.env.NEXT_PUBLIC_SITE_URL}/auth/auth-code-error`
+  );
 }
